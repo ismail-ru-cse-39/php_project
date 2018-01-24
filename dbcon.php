c7ce08748d3ce72e5782a3b96c1e566972470301
ismail-ru-cse-39 <shaikat.ru.39@gmail.com>
2018-01-23 22:50:33 -0800

    index.php has completed

---
+++ dbcon.php
@@ -0,0 +1,11 @@
+<?php
+
+	$con = mysqli_connect("localhost", "root", "123","sms");
+
+	if($con == false){
+		echo "Connection failed";
+	}
+
+	
+
+?>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html lang = "en_US">
+<head>
+
+	<meta charset = "UTF-8">
+	<title>Student Management System</title>
+</head>
+<body>
+
+	<h1 align = "center">Welcome to Student Management System</h1>
+	<h3 align = "right" style = "margin-right:20px;"><a href = "login.php">Admin Login</a></h3>
+
+	<form method = "post" action = "index.php">
+	<table style = "width : 50%;"  align = "center" border = "1">
+
+		<tr>
+			<td colspan = "2" align = "center">Student Information</td>
+
+		</tr>
+
+		<tr>
+			<td align = "right">Choose Standard</td>
+
+			<td>
+				<select name = "std">
+					<option values = "1">1st</option>
+					<option values = "2">2nd</option>
+					<option values = "3">3rd</option>
+					<option values = "4">4rth</option>
+					<option values = "5">5th</option>
+					<option values = "6">6th</option>
+
+				</select>
+			</td>
+		</tr>
+
+		<tr>
+
+			<td align = "right">Enter Rollno</td>
+
+			<td>
+				 <input type = "text" name = "rollno" required>
+			</td>
+		</tr>	
+
+		<tr>
+
+			<td colspan = "2" align = "center"><input type = "submit" name = "submit" value = "Show Info"></td>
+		</tr>	
+	</table>
+
+</form>
+
+</body>
